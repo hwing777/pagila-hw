ac5ece9cb5032f714a303e37299f6335c19e3d0d
Heather Wing <hwing777@gmail.com>
2021-02-17 18:47:00 -0800

    Clean up 15

--- sql/15.sql
+++ sql/15.sql
@@ -24,22 +24,3 @@ LEFT JOIN (
 ) AS final_category_count ON category.category_id=final_category_count.category_id
 ORDER BY category.name;
 
-
-
-
-/*
-SELECT film_cats.name, count(*) as "sum"
-FROM (
-    SELECT film_category.film_id, category.category_id, category.name
-    FROM film_category
-    LEFT JOIN category ON film_category.category_id=category.category_id
-) AS film_cats
-JOIN (
-    SELECT film.film_id, language.name
-    FROM film
-    LEFT JOIN language ON film.language_id=language.language_id
-    GROUP BY film_id, name
-    HAVING name='English'
-) AS film_langs ON film_langs.film_id=film_cats.film_id
-GROUP BY film_cats.name;
-*/
